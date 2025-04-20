5177189f1c9601d274708e8144ecd5e40f9a0e1a
MorningStarGG <spyder2k5@gmail.com>
2025-04-19 19:02:25 -0500

    These hardcoded values for updates and downloads are not a good practise I hope these are the last that override the new WebUI

--- server/src/main/kotlin/suwayomi/tachidesk/graphql/types/WebUIUpdateType.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/graphql/types/WebUIUpdateType.kt
@@ -63,9 +63,9 @@ enum class WebUIFlavor(
 ) {
     WEBUI(
         "WebUI",
-        "https://github.com/Suwayomi/Suwayomi-WebUI-preview",
-        "https://raw.githubusercontent.com/Suwayomi/Suwayomi-WebUI/master/versionToServerVersionMapping.json",
-        "https://api.github.com/repos/Suwayomi/Suwayomi-WebUI-preview/releases/latest",
+        "https://github.com/MorningStarGG/Suwayomi-WebUI-preview",
+        "https://raw.githubusercontent.com/MorningStarGG/Suwayomi-WebUI/master/versionToServerVersionMapping.json",
+        "https://api.github.com/repos/MorningStarGG/Suwayomi-WebUI-preview/releases/latest",
         "Suwayomi-WebUI",
     ),
 
