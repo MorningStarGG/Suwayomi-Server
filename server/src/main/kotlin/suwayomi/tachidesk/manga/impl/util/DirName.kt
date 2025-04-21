4e442b4d00ab74b81dbf320d3d5c84bfa5c4af76
MorningStarGG <spyder2k5@gmail.com>
2025-04-21 15:17:43 -0500

    Minor code formatting to trigger build

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/DirName.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/util/DirName.kt
@@ -11,19 +11,17 @@ import java.io.File
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import suwayomi.tachidesk.manga.impl.util.source.GetCatalogueSource
 import suwayomi.tachidesk.manga.model.table.ChapterTable
 import suwayomi.tachidesk.manga.model.table.MangaTable
 import suwayomi.tachidesk.server.ApplicationDirs
 import suwayomi.tachidesk.server.serverConfig
 import uy.kohesive.injekt.injectLazy
-import xyz.nulldev.androidcompat.util.SafePath
 
 private val applicationDirs: ApplicationDirs by injectLazy()
 
 private fun getMangaDir(mangaId: Int): String {
     val mangaEntry = getMangaEntry(mangaId)
-    
+
     // Use the format from config
     val format = serverConfig.mangaFolderFormat.value
     val variables = FormatHelper.createMangaVariables(mangaEntry)
@@ -31,12 +29,14 @@ private fun getMangaDir(mangaId: Int): String {
 }
 
 private fun getChapterDir(
-    mangaId: Int,
-    chapterId: Int,
+        mangaId: Int,
+        chapterId: Int,
 ): String {
-    val chapterEntry = transaction { ChapterTable.selectAll().where { ChapterTable.id eq chapterId }.first() }
+    val chapterEntry = transaction {
+        ChapterTable.selectAll().where { ChapterTable.id eq chapterId }.first()
+    }
     val mangaEntry = getMangaEntry(mangaId)
-    
+
     // Use the format from config
     val format = serverConfig.chapterFolderFormat.value
     val variables = FormatHelper.createChapterVariables(chapterEntry, mangaEntry)
@@ -45,39 +45,41 @@ private fun getChapterDir(
     return getMangaDir(mangaId) + "/$chapterDir"
 }
 
-fun getThumbnailDownloadPath(mangaId: Int): String = applicationDirs.thumbnailDownloadsRoot + "/$mangaId"
+fun getThumbnailDownloadPath(mangaId: Int): String =
+        applicationDirs.thumbnailDownloadsRoot + "/$mangaId"
 
-fun getMangaDownloadDir(mangaId: Int): String = applicationDirs.mangaDownloadsRoot + "/" + getMangaDir(mangaId)
+fun getMangaDownloadDir(mangaId: Int): String =
+        applicationDirs.mangaDownloadsRoot + "/" + getMangaDir(mangaId)
 
 fun getChapterDownloadPath(
-    mangaId: Int,
-    chapterId: Int,
+        mangaId: Int,
+        chapterId: Int,
 ): String = applicationDirs.mangaDownloadsRoot + "/" + getChapterDir(mangaId, chapterId)
 
 fun getChapterCbzPath(
-    mangaId: Int,
-    chapterId: Int,
+        mangaId: Int,
+        chapterId: Int,
 ): String = getChapterDownloadPath(mangaId, chapterId) + ".cbz"
 
 fun getChapterCachePath(
-    mangaId: Int,
-    chapterId: Int,
+        mangaId: Int,
+        chapterId: Int,
 ): String = applicationDirs.tempMangaCacheRoot + "/" + getChapterDir(mangaId, chapterId)
 
 /** return value says if rename/move was successful */
 fun updateMangaDownloadDir(
-    mangaId: Int,
-    newTitle: String,
+        mangaId: Int,
+        newTitle: String,
 ): Boolean {
     val mangaEntry = getMangaEntry(mangaId)
 
     // Create variables for old path
     val oldVariables = FormatHelper.createMangaVariables(mangaEntry)
-    
+
     // Create variables for new path with updated title
     val newVariables = oldVariables.toMutableMap()
     newVariables["title"] = newTitle
-    
+
     // Format paths using the configured format
     val format = serverConfig.mangaFolderFormat.value
     val oldMangaDir = FormatHelper.formatString(format, oldVariables)
@@ -96,4 +98,6 @@ fun updateMangaDownloadDir(
     }
 }
 
-private fun getMangaEntry(mangaId: Int): ResultRow = transaction { MangaTable.selectAll().where { MangaTable.id eq mangaId }.first() }
+private fun getMangaEntry(mangaId: Int): ResultRow = transaction {
+    MangaTable.selectAll().where { MangaTable.id eq mangaId }.first()
+}
